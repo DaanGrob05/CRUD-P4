2b81d0c3541021fdf19824f9c4f9e89a834d428d
ivan-k2310 <94127896+ivan-k2310@users.noreply.github.com>
2022-05-13 13:48:23 +0200

    opzet index (#3)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,11 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+        href="https://fonts.googleapis.com/css2?family=Bakbak+One&family=Noto+Sans:ital,wght@1,700&family=Open+Sans:ital,wght@1,500&family=Playfair+Display:ital,wght@1,900&family=Roboto&display=swap"
+        rel="stylesheet"
+        />
         <title> {{ config('app.name') }}</title>
 
         <!-- Fonts -->
@@ -12,6 +16,7 @@
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
     </head>
+    <!-- Authenticatie, laten staan voor nu -->
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
@@ -29,6 +34,66 @@
             @endif
         </div>
 
-        <p class="test">Hoi</p>
+        <!-- Main content -->
+        <div class="landingSpacer">
+      <nav>
+        <!--  -->
+      </nav>
+      <div class="landing">
+        <div class="landingContent">
+          <div class="flightsFormCon">
+            <div class="flightsNav">
+              <div class="flightsNavItem">
+                <a>example</a>
+              </div>
+              <div class="flightsNavItem">
+                <a>example</a>
+              </div>
+              <div class="flightsNavItem">
+                <a>example</a>
+              </div>
+              <div class="flightsNavItem">
+                <a>example</a>
+              </div>
+            </div>
+            <div class="formTitle">
+              <p>Flight</p>
+            </div>
+            <form></form>
+          </div>
+          <div class="extraInfo">
+            <!--  -->
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="contentSpacer">
+      <!-- temp name -->
+      <div class="content">
+        <div class="contentHeader">
+          <h1></h1>
+        </div>
+        <div class="contentBody"></div>
+      </div>
+    </div>
+    <div class="contentSpacer">
+      <!-- temp name -->
+      <div class="content">
+        <div class="contentHeader">
+          <h1></h1>
+        </div>
+        <div class="contentBody"></div>
+      </div>
+    </div>
+    <div class="contentSpacer">
+      <!-- temp name -->
+      <div class="content">
+        <div class="contentHeader">
+          <h1></h1>
+        </div>
+        <div class="contentBody"></div>
+      </div>
+    </div>
+    <footer></footer>
     </body>
 </html>

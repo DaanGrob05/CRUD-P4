a38e6566fb003b5c38c2fd396c9244179a29efe5
DaanGrob05 <d.grob05@gmail.com>
2022-05-18 13:08:18 +0200

    isAdmin middleware en userseed aangemaakt

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert(
+            [
+                'name' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('adminPass'),
+                'is_admin' => 1,
+            ]
+        );
+        DB::table('users')->insert(
+            [
+                'name' => 'user',
+                'email' => 'user@gmail.com',
+                'password' => Hash::make('userPass'),
+                'is_admin' => 0,
+            ]
+        );
+    }
+}

ebc2d31205578c29f176be1809ba03b0ee6b31d5
ivan-k2310 <ivankeller6@gmail.com>
2022-05-19 10:25:40 +0200

    backgroundslider needs to be cashed tho

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,6 @@
 const mix = require("laravel-mix");
 
+mix.js("resources/js/slider.js", "public/js/slider.js");
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -11,12 +12,12 @@ const mix = require("laravel-mix");
  |
  */
 
-mix.js("resources/js/app.js", "public/js").postCss(
-    "resources/css/app.css",
-    "public/css",
-    [
-        //
-    ]
-);
+// mix.js("resources/js/app.js", "public/js").postCss(
+//     "resources/css/app.css",
+//     "public/css",
+//     [
+//         //
+//     ]
+// );
 
 mix.sass("resources/scss/app.scss", "public/css");

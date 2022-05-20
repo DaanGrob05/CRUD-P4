de2324736bcfb24fa998b1a269851caabbcc4a30
DaanGrob05 <86193398+DaanGrob05@users.noreply.github.com>
2022-05-20 08:53:42 +0200

    Admin begin (#7)
    * isAdmin middleware en userseed aangemaakt
    
    * welcome page aangepast
    
    * laatste updates

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,5 +22,24 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::get('/home', function () {
+    if (Auth::user()->is_admin == 1) {
+        return redirect()->route('admin');
+    } else {
+        return redirect()->route('dashboard');
+    }
+})->name('home');
+
+Route::get('/admin', [AdminController::class, 'dashboard'])->name('admin');
+// TODO : Add reizen in AdminController
+Route::get('/admin/reizen', [AdminController::class, 'reizen'])->name('admin.reizen');
+Route::get('/admin/reizen/{id}', [AdminController::class, 'reizen_detail'])->name('admin.reizen.detail');
+Route::get('/admin/reizen/{id}/edit', [AdminController::class, 'reizen_edit'])->name('admin.reizen.edit');
+Route::post('/admin/reizen/{id}/edit', [AdminController::class, 'reizen_update'])->name('admin.reizen.update');
+Route::get('/admin/reizen/{id}/delete', [AdminController::class, 'reizen_delete'])->name('admin.reizen.delete');
+
+// TODO : Add ReisController 
+// TODO : Add ReviewController
 
 require __DIR__ . '/auth.php';

d984907aef15cc05310c0e985567eac07f085af4
DaanGrob05 <86193398+DaanGrob05@users.noreply.github.com>
2022-06-01 13:02:16 +0200

    login en register aangepast, basis voor trips gemaakt (#13)
    Trips index en show is gemaakt, show werkt nog niet. Login en register bleken niet te werken dus die zijn aangepast. De branch word nu gemerged omdat dit belangrijke onderdelen waren

---
+++ app/Http/Controllers/TripController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Trip;
+use Illuminate\Http\Request;
+
+class TripController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('isAdmin', ['except' => ['index', 'show']]);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $trips = Trip::all();
+        // dd($trips);
+        return view('reizen.index')->with('trips', $trips);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  Trip  $trip
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Trip $trip)
+    {
+        return view('reizen.show')->with('trip', $trip);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}

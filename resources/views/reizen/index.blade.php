b61e3e736a82cb58ba5d29527b387cab0b4c2f59
DaanGrob05 <d.grob05@gmail.com>
2022-06-17 09:06:41 +0200

    nav aan user reizen toegevoegd

--- resources/views/reizen/index.blade.php
+++ resources/views/reizen/index.blade.php
@@ -18,7 +18,9 @@
     </head>
     <body class="antialiased">
         <div class="tripsspacer">
-            <x-nav></x-nav>
+            <x-nav
+                id="blueNav">
+            </x-nav>
             <div class="searchSpacer">
                 <div class="searchTitleCon">
                     <a class="searchFormTitle">Search by destination</a>

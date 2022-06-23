710bee92870e2730c45d6b88763ebc75c8edeceb
ivan-k2310 <ivankeller6@gmail.com>
2022-06-23 22:55:08 +0200

    admin-show-, -edit-, -create-pages css done, function broke need to be fixed

--- resources/views/admin/reizen.blade.php
+++ resources/views/admin/reizen.blade.php
@@ -2,14 +2,67 @@
     id="blueNav">
 </x-nav>
 
-<h1>Admin Reizen Pagina</h1>
 
-<button><a href="{{ route('reizen.create') }}">Nieuwe reis aanmaken</a></button>
+<div class="adminTripsSpacer">
+    <div class="adminTitleCon">
+        <h1 class="adminTitle">Trips overview</h1>
+        <h3 class="adminSubTitle">all info about trip is stated here</h3>
+    </div>
+    <div class="createTripCon">
+        <button class="generalButton"><a  href="{{ route('reizen.create') }}">Create new Trip</a></button>
+    </div>
+    <div class="tripsCon">
+        @forelse ($trips as $trip)
+        
+            <div class="tripConAdmin">
+                <div class="tripInfo">
+                    <div class="tripName">
+                        <p>{{ $trip->trip_name }}</p>
+                    </div>
+                    <div class="tripDescription">
+                        <p>{{ $trip->full_description }}</p>
+                    </div>
+                    <div class="tripDescription">
+                        <p>{{ $trip->small_description }}</p>
+                    </div>
+                    <div class="tripDate">
+                        <p>{{ $trip->startDate }} / {{ $trip->endDate }}</p>
+                    </div>
+                    <div class="moreInfo">
+                        <p>{{ $trip->start_location }} / {{ $trip->destination }}</p>
+                    </div>
+                    <div class="moreInfo">
+                        <p>type of trip: {{ $trip->type_of_trip }}</p>
+                    </div>
+                    <div class="moreInfo">
+                        <p>{{ $trip->hotel }}</p>
+                    </div>
+                    <div class="moreInfo">
+                        <p class="tripPrice">€{{ $trip->price }},-</p>
+                    </div>
+                    
 
-@forelse ($trips as $trip)
-<a href="{{ route('admin.reizen.show', $trip->trip_id) }}">
-    <p>{{ $trip->small_description }}</p>
-</a>
-@empty
-<p>Er zijn geen reizen gevonden.</p>
-@endforelse
+                    <div class="moreInfo">
+                        <button class="generalButton">
+                            <a  href="{{ route('reizen.edit', $trip->trip_id) }}">Edit</a>
+                        </button>
+                        <form action="{{ route('reizen.destroy', $trip->trip_id) }}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="generalButton" onclick="return confirm('Weet je zeker dat je deze reis wilt verwijderen?')">
+                               <a>Delete</a> 
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        @empty
+        <p>Er zijn geen reizen gevonden.</p>
+        @endforelse
+    </div>  
+    <button class="generalButton">
+        <a  href="{{ route('admin') }}">back</a>
+    </button>  
+</div>
+
+

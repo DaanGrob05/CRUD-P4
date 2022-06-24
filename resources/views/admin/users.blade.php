3ab363a80282984d853249d2033ec81d582f82c2
ivan-k2310 <ivankeller6@gmail.com>
2022-06-24 21:19:57 +0200

    users page css done, sumbit for edit needs to redirect to all user page

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -1,13 +1,51 @@
 <x-nav
     id="blueNav">
 </x-nav>
-
-@foreach ($users as $user)
-    <div class="">
-        <a href="{{ route('admin.users.show', $user->id) }}">
-            <p>{{ $user->name }}</p>
-            <p>{{ $user->email }}</p>
-            <br>    
-        </a>
+<div class="adminSpacer">
+    <div class="adminTitleCon">
+        <h1 class="adminTitle">Users</h1>
+        <h3 class="adminSubTitle">All users are stated here</h3>
     </div>
-@endforeach
+    <div class="adminUsersCon">
+        <div class="usersCon">
+            <div class="usersColumns">
+                <div class="usersColumn">
+                    <h3 class="usersColumnTitle">Name</h3>
+                </div>
+                <div class="usersColumn">
+                    <h3 class="usersColumnTitle">Email</h3>
+                </div>
+                <div class="usersColumn">
+                    <h3 class="usersColumnTitle">Edit</h3>
+                </div>
+                <div class="usersColumn">
+                    <h3 class="usersColumnTitle">Delete</h3>
+                </div>
+            </div>
+            <div class="userItemsCon">
+                @foreach ($users as $user)
+                <div class="userItemCon">
+                    <div class="userInfoCon">
+                        <p>{{ $user->name }}</p>
+                    </div>
+                    <div class="userInfoCon">
+                        <p>{{ $user->email }}</p>
+                    </div>
+                    <div class="userInfoCon">
+                        <button class="SgeneralButton">
+                            <a href="{{ route('admin.users.edit', $user->id) }}">Edit</a>
+                        </button>
+                    </div>
+                    <div class="userInfoCon">
+                        <form action="{{ route('admin.users.delete', $user->id) }}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button class="SgeneralButton" type="submit" onclick="return confirm('Are you sure you want to delete this user?')"><a>Delete</a></button>
+                        </form>
+                    </div>
+                </div>
+                @endforeach   
+            </div>
+        </div>
+    </div>
+</div>

89c59059fd95045e1d96639925f90136b513455d
ivan-k2310 <ivankeller6@gmail.com>
2022-06-26 18:42:03 +0200

    contact page done, should be looked at after nav-bar merge

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,48 +2,80 @@
     id="blueNav">
 </x-nav>
 <div class="contactSpacer">
-    <div class="formCon">
-        <div class="formHeader">
-
+    <div class="contactContentSpacer">
+        <div class="contactTitle">
+            <h1>Contact us</h1>
+            <h3>If you have any questions about our products or services, please contact us.</h3>
+            <h4>You could also E-mail us at EXAMPLE@gmail.com</h4>
         </div>
+        <div class="contactUsFormCon">
+            <div class="contactUsForm">
+                <form action="{{ route('contact.store') }}" method="POST">
+                    @csrf
+                    <div class="contactInput_Label">
+                        <label class="contactLabel" for="name">Name *</label>
+                            <x-contact-input 
+                            class="contactInput"
+                            type="text"
+                            name="name"
+                            field="name"
+                            label=""
+                            :value="@old('name', $contact->name)"
+                        ></x-contact-input>
+                    </div>
+                    <div class="contactInput_Label">
+                        <label class="contactLabel" for="email">Email *</label>
+                        <x-contact-input
+                            class="contactInput" 
+                            type="text"
+                            name="email"
+                            field="email"
+                            label=""
+                            :value="@old('email', $contact->email)"
+                        ></x-contact-input>
+                    </div>
+                    <div class="contactInput_Label">
+                        <label class="contactLabel" for="message">Company</label>
+                        <x-contact-input
+                            class="contactInput" 
+                            type="text"
+                            name="company"
+                            field="company"
+                            label=""
+                            :value="@old('company', $contact->company)"
+                        ></x-contact-input>
+                    </div>
+                    <div class="contactInput_Label">
+                        <label class="contactLabel" for="message">Message *</label>
+                        <x-contact-input
+                            class="contactInput" 
+                            type="text"
+                            name="message"
+                            field="message"
+                            label=""
+                            :value="@old('message', $contact->message)"
+                        ></x-contact-input>
+                    </div>
+                    <button type="submit" class="SgeneralButton"><a>Submit</a></button>
+                </form>
+            </div> 
+        </div> 
+    </div>
+</div>
+<x-footer></x-footer>
 
-        @if (session('status'))
+
+{{-- @if (session('status'))
             <div class="alert alert-success">
                 {{ session('status') }}
             </div>
         @endif
         <form action="{{route('contact.store') }}" method="POST">
             @csrf
-            <x-contact-input 
-                type="text"
-                name="name"
-                field="name"
-                label="Name *"
-                :value="@old('name', $contact->name)"
-            ></x-contact-input>
-            <x-contact-input 
-                type="text"
-                name="email"
-                field="email"
-                label="Email *"
-                :value="@old('email', $contact->email)"
-            ></x-contact-input>
-            <x-contact-input 
-                type="text"
-                name="company"
-                field="company"
-                label="Company"
-                :value="@old('company', $contact->company)"
-            ></x-contact-input>
+            
+            
+            
 
-            <x-contact-input 
-                type="text"
-                name="message"
-                field="message"
-                label="Message *"
-                :value="@old('message', $contact->message)"
-            ></x-contact-input>
+            
             <button type="submit">Send Message</button>
-        </form>
-    </div>
-</div>
+        </form> --}}

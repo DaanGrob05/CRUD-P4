27ec20eb6db361438640e20f72155695c1ab4a58
ivan-k2310 <ivankeller6@gmail.com>
2022-06-27 21:57:32 +0200

    edit.profile css done, function also works

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -1,12 +1,67 @@
 <x-nav
     id="blueNav">
 </x-nav>
+<div class="reviewSpacer">
+    <div class="reviewCon">
+        <div class="reviewForms">
+            <div class="reviewHeader">
+                    <h1>Edit Profile</h1>
+            </div>
+            <div class="reviewFormCon">
+                <form action="{{ route('profile.update')}}"" method="post">
+                @csrf
+                @method('PUT')
+                    <div class="reviewFormItem">
+                        <x-trip-input
+                            class="reviewFormItemInput"
+                            label="Name: "
+                            field="name"
+                            name="name"
+                            value="{{ Auth::user()->name }}"
+                        ></x-trip-input>
+                    </div>
+                    <div class="reviewFormItem">
+                        <x-trip-input
+                            class="reviewFormItemInput"
+                            label="Email: "
+                            field="email"
+                            name="email"
+                            value="{{ Auth::user()->email }}"
+                        ></x-trip-input>
+                    </div>
+                    <div class="reviewFormItem">
+                        <x-trip-input
+                            class="reviewFormItemInput"
+                            label="Password: "
+                            field="password"
+                            name="password"
+                            type="password"
+                        ></x-trip-input>
+                    </div>
+                    <div class="loginButtonCon">
+                        <button class="loginButton">
+                            <a>Submit</a>
+                        </button>
+                    </div>
+            </div>
+        </div>
+    </div>
+    <button class="generalButton">
+        <a  href="{{ route('profile') }}">Back</a>
+    </button>   
+    
+</div>
+<x-footer></x-footer>
 
-<form action="{{ route('profile.update')}}" method="POST">
+
+
+
+
+
+{{-- <form action="{{ route('profile.update')}}" method="POST">
     @csrf
-    @method('PUT')
+    
 
-    {{-- Name --}}
     <div class="">
         <x-general-input
             label="Name"
@@ -16,7 +71,6 @@
         ></x-general-input>
     </div>
 
-    {{-- Email --}}
     <div class="">
         <x-general-input
             label="Email"
@@ -26,7 +80,7 @@
         ></x-general-input>
     </div>
 
-    {{-- Password --}}
+
     <div class="">
         <x-general-input
             label="Password"
@@ -36,4 +90,4 @@
         ></x-general-input>
     </div>
     <button type="submit">Update</button>
-</form>
+</form> --}}

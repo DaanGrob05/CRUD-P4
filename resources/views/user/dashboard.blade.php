ce883964cde82f7aaaa2ec1f835f52f5b299d6ab
ivan-k2310 <ivankeller6@gmail.com>
2022-06-27 11:40:58 +0200

    begin layout dashboard

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -4,20 +4,34 @@
 <div class="userProfileSpacer">
     <div class="userProfileContentSpacer">
         <div class="userProfileTitle">
-
             <h1>Hello {{ Auth::user()->name }}</h1>
         </div>
-        <div class="userItem">
-            
-            <div class="">
-                    <a href="{{ route('profile.bookings') }}">Bookings</a>
+        <div class="userProfileContentCon">
+            <div class="userProfileInfo">
+                <div class="userProfileInfoTitle">
+                    <h2>Your Profile</h2>
                 </div>
-        </div>
-        <div class="userItem">
-            <div class="">
-                <a href="{{ route('profile.edit') }}">Edit Profile Information</a>
+                <div class="userProfileBody">
+                    <div class="userProfileDetailCon">
+                        <div class="userProfile">
+                            <h3>Name</h3>
+                        </div>
+                        <div class="userProfile">
+                            <p>{{ Auth::user()->name }} Sins</p>
+                        </div>
+                    </div>
+                    <div class="userProfile">
+                        <div class="userProfile">
+                            <h3>Email</h3>
+                        </div>
+                        <div class="userProfile">
+                            <p>{{ Auth::user()->email }}</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
+        
     </div>
     <form method="POST" action="{{ route('logout') }}">
         @csrf
@@ -31,3 +45,14 @@
 </div>
 <x-footer></x-footer>
 
+{{-- <div class="userItem">
+            
+            <div class="">
+                    <a href="{{ route('profile.bookings') }}">Bookings</a>
+                </div>
+        </div>
+        <div class="userItem">
+            <div class="">
+                <a href="{{ route('profile.edit') }}">Edit Profile Information</a>
+            </div>
+        </div> --}}
